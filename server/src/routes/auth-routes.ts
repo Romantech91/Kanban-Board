11476b1b6517aa34e63889f60789f4f0cb408cd6
Victor Roman <Romantech91>
2024-09-11 21:29:58 -0700

    server auth-routes.ts completed

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -4,6 +4,22 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username});
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+    const token = jwt.sign({ id: user._id, username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
   // TODO: If the user exists and the password is correct, return a JWT token
 };
 
