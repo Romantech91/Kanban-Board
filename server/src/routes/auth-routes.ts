a8ca6fe17c8ad403f18353b661121be5be720184
Victor Roman <Romantech91>
2024-09-11 21:39:32 -0700

    updated auth.ts in server/src/routes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,22 +5,23 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
-  try {
-    const user = await User.findOne({ username});
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid password' });
-    }
-    const token = jwt.sign({ id: user._id, username: user.username }, SECRET_KEY, { expiresIn: '1h' });
-    res.json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
+
+  const user = await User.findOne({
+    where: { username },
+  });
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
   }
-  // TODO: If the user exists and the password is correct, return a JWT token
+
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  return res.json({ token });
 };
 
 const router = Router();
