20281bd9aeb30d1b5c27d07fc68fdcea77cc33c6
Victor Roman <Romantech91>
2024-09-15 22:14:14 -0700

    api authapi.tsx updated

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,6 +1,24 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+    if (!response.ok) {
+      const errorMessage = await response.json();
+      throw new Error(errorMessage.message);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error in login', error);
+    return Promise.reject('Something went wrong');
+  }
   // TODO: make a POST request to the login route
 }
 
