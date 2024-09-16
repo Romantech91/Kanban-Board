82ccb70752884ef15077bb8bf40e1813f65e3c90
Victor Roman <Romantech91>
2024-09-15 21:28:42 -0700

    utils auth.ts updated.

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,28 +1,46 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+ import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
   getProfile() {
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
     // TODO: return the decoded token
   }
 
   loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
     // TODO: return a value that indicates if the user is logged in
   }
   
   isTokenExpired(token: string) {
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded.exp && decoded.exp * 1000 < Date.now()) {
+        return true;
+      }
+      return false;
     // TODO: return a value that indicates if the token is expired
+  } catch (err) {
+    return false;
   }
+}
 
-  getToken(): string {
+  getToken(): string | null {
+    return localStorage.getItem('jwtToken') || '';
     // TODO: return the token
   }
 
   login(idToken: string) {
+    localStorage.setItem('jwtToken', idToken);
+    window.location.assign('/');
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
   }
 
   logout() {
+    localStorage.removeItem('jwtToken');
+    window.location.assign('/login');
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
   }
